8dae6a7f6462977c007ba45700e827caae24954c
Bright Onapito <onabright@gmail.com>
2019-11-20 15:07:00 +0300

    Delete project_data.php

--- project_data.php
+++
@@ -1,8 +0,0 @@
-<?php
-$sql_query = "SELECT * FROM licensed_projects ORDER BY year_licensed DESC ";
-$resultset = mysqli_query($conn, $sql_query) or die("database error:". mysqli_error($conn));
-$data_records = array();
-while( $rows = mysqli_fetch_assoc($resultset) ) {
-	$data_records[] = $rows;
-}
-?>
